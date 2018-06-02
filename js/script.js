e1cc0b4a9ada3703c341ebbb46fdefc6fb13cfef
Your-Full-Name <your-email-address>
2018-06-02 19:01:09 +0200

    Added NYTimes api functionality

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ function loadData() {
     $wikiElem.text("");
     $nytElem.text("");
 
-    // load streetview=
+    // load  Google Streetview
     var streetStr = $('#street').val();
     var cityStr = $('#city').val();
     var address = streetStr + ', ' + cityStr;
@@ -21,6 +21,21 @@ function loadData() {
     var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
     $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
 
+    // load NYTimes AJAX requests
+    var nytimesUrl = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q='+ cityStr + '&sort=newest&api-key=927531d5c2df4b8299c80bac09ac506b'
+    $.getJSON(nytimesUrl, function(data){
+
+        $nytHeaderElem.text('New York Times Articles About ' + cityStr);
+
+        articles = data.response.docs;
+        for (var i = 0; i < articles.length; i++) {
+            var article = articles[i];
+            $nytElem.append('<li class="article">'+
+                '<a href="'+article.web_url+'">'+article.headline.main+'</a>'+
+                '<p>'+ article.snippet + '</p>'+
+                '</li>');
+              };
+    })
     return false;
 };
 
