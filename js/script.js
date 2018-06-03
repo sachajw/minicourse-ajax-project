95a223f77c60457f0f37ecdfa608f606a9d0e529
Your-Full-Name <your-email-address>
2018-06-03 11:56:04 +0200

    Added Wikipedia API Funtionality

--- js/script.js
+++ js/script.js
@@ -21,7 +21,7 @@ function loadData() {
     var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
     $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
 
-    // load NYTimes
+    // load NYTimes AJAX request
     var nytimesUrl = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q='+ cityStr + '&sort=newest&api-key=927531d5c2df4b8299c80bac09ac506b'
     $.getJSON(nytimesUrl, function(data){
 
@@ -38,6 +38,25 @@ function loadData() {
     }).error(function(e){
         $nytHeaderElem.text('New York Times Articles Could Not Be Loaded');
     });
+
+    //Wikipedia AJAX request
+    var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search='
+        + cityStr + ' &format=json&callback=wikiCallback';
+
+    $.ajax({
+        url:wikiUrl,
+        dataType: "jsonp",
+        //jsonp: "callback",
+        success: function( response ) {
+            var articleList = response[1];
+
+            for (var i = 0; i < articleList.length; i++) {
+                articleStr = articleList[i];
+                var url = 'http://en.wikipedia.org/wiki/' + articleStr;
+                $wikiElem.append('<li><a href="' + url + '">' + articleStr + '</a></li>');
+            }
+        }
+    })
     return false;
 };
 
